d8ada96f35846850d59333793f96297cbd1d5ab3
Ria Susan Viju <ria.susanviju@finastra.com>
2022-01-17 21:55:43 +0530

    Adding src

---
+++ src/app/models/customer.ts
@@ -0,0 +1,13 @@
+export interface Customer {
+  firstName: string;
+  lastName: string;
+  countryOfResidency: string;
+  addline1: string;
+  addline2: string;
+  addline3: string;
+  addline4: string;
+  postalCode: string;
+  buildingNumber: string;
+  phoneNumber: string;
+  emailAddress: string;
+}
